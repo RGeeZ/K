5d24cdb20a5316ddd4b86a2d5182d1f259d12966
Rishabh Giri <2000rishabhgiri@gmail.com>
2022-11-20 21:03:32 +0530

    Added Now

--- reactapp/portfolio/src/container/home/Middle.js
+++ reactapp/portfolio/src/container/home/Middle.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Middle() {
     return (
         <div className='container'>
-            <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" data-animate-effect="fadeIn">
+            {/*<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box" data-animate-effect="fadeIn">
 					<h2>We are Offering the Best Real Estate Deals</h2>
 					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-				</div>
+				</div>*/}
         </div>
     )
 }
